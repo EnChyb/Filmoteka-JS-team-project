25b41ab575c6f8d68c04e416c8c073320cfff104
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-03-10 13:09:23 +0100

    Update watched.js

--- src/js/watched.js
+++ src/js/watched.js
@@ -1,2 +1,3 @@
 const galleryOfMovies = document.querySelector('.movie-gallery');
 const cardOfMovie = document.querySelector('.movie-card');
+const addToWatched = document.querySelector('.add-watched');

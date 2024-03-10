fddcf9b95780a22241cc22f2c54cdb827aca6231
Karolina <143939666+karolinawojcik0@users.noreply.github.com>
2024-03-10 17:21:28 +0100

    backdrop changes

--- src/js/backdrop.js
+++ src/js/backdrop.js
@@ -1,13 +1,14 @@
-const backdrop = document.querySelector('.modal-filmoteka');
-const body = document.querySelector('body');
-
-export function showBackdrop() {
-  backdrop.classList.remove('is-hidden');
-  body.style = `overflow-y: hidden`;
-}
-
-export function closeBackdrop() {
-  backdrop.classList.add('is-hidden');
-  backdrop.innerHTML = '';
-  body.style = `overflow-y: overlay`;
-}
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-subscribe]"),
+      closeModalBtn: document.querySelector("[data-modal-close-window]"),
+      modal: document.querySelector("[data-modal-window]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();

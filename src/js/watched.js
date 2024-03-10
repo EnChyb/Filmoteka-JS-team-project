26929878b3c35db6b4bb487a5e1085bfec355ea9
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-03-10 12:52:54 +0100

    Update watched.js

--- src/js/watched.js
+++ src/js/watched.js
@@ -1,3 +1,5 @@
 const addToWatched = document.querySelector('.add-watched');
+const galleryOfMovies = document.querySelector('.movie-gallery');
+const cardOfMovies = document.querySelector('.movie-card');
 
 addToWatched.addEventListener('click', () => {});

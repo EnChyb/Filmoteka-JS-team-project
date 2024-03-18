b9327f0fca95118631642bac49a56baccfe444bb
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-03-18 17:02:34 +0100

    upd

--- src/js/queue.js
+++ src/js/queue.js
@@ -1,19 +1,5 @@
 import { searchDetails } from './database';
 
-// inicjalizacja biblioteki firebase
-const firebaseConfig = {
-  apiKey: 'AIzaSyDDpwcjcKhe_urNJExT9mupeVvY7ZU4amc',
-  authDomain: 'filmoteka-99859.firebaseapp.com',
-  projectId: 'filmoteka-99859',
-  storageBucket: 'filmoteka-99859.appspot.com',
-  messagingSenderId: '211928039404',
-  appId: '1:211928039404:web:d0752ca154999a60d40ac9',
-  measurementId: 'G-127B3WS3FV',
-};
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
-const storage = firebase.storage();
 
 const galleryOfMovies = document.querySelector('.movie-gallery');
 const addToQueueButton = document.querySelector('.add-queue');

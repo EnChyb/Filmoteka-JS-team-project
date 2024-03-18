3303eb5ee21d3878ef53da7720ba3eed766e4cfa
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-03-18 19:08:11 +0100

    upd

--- src/js/watched.js
+++ src/js/watched.js
@@ -1,20 +1,21 @@
 import Notiflix from 'notiflix';
+import { searchDetails } from './database';
+import { openModal } from './modal';
 
-const addToWatched = document.querySelector('.add-watched');
-const addToQueue = document.querySelector('.add-queue');
+let ID;
 
-const localStorage = movieId => {
+const localStorage = event => {
   const addedToWatched = () => {
-    Notiflix.Notify.info(`The movie: "${movieId.title}" has been added to watched`);
+    Notiflix.Notify.info(`The movie has been added to watched`);
   };
   const addedToQueue = () => {
-    Notiflix.Notify.info(`The movie: "${movie.data.title}" has been added to the queue`);
+    Notiflix.Notify.info(`The movie has been added to the queue`);
   };
   const removeFromWatched = () => {
-    Notiflix.Notify.info(`The movie: "${movie.data.title}" has been removed from watched`);
+    Notiflix.Notify.info(`The movie has been removed from watched`);
   };
   const removeFromQueue = () => {
-    Notiflix.Notify.info(`The movie: "${movie.data.title}" has been removed from the queue`);
+    Notiflix.Notify.info(`The movie has been removed from the queue`);
   };
   const moviesWatched = JSON.parse(localStorage.getItem('movies-watched')) || [];
   const moviesQueue = JSON.parse(localStorage.getItem('movies-queue')) || [];
@@ -23,12 +24,12 @@ const localStorage = movieId => {
   const addQueueRef = document.querySelector('.add-queue');
 
   if (localStorage.length > 0) {
-    if (moviesWatched.find(item => item.id === film.data.id)) {
+    if (moviesWatched.find(item => item.id === ID)) {
       addWatchedRef.textContent = 'remove from watched';
     }
   }
   if (localStorage.length > 0) {
-    if (moviesQueue.find(item => item.id === film.data.id)) {
+    if (moviesQueue.find(item => item.id === ID)) {
       addQueueRef.textContent = 'remove from queue';
     }
   }
@@ -36,29 +37,34 @@ const localStorage = movieId => {
   addWatchedRef.addEventListener('click', onWatchedClick);
   addQueueRef.addEventListener('click', onQueueClick);
 
-  function onWatchedClick() {
-    console.log(film);
-    if (!moviesWatched.find(item => item.id === film.data.id)) {
-      moviesWatched.push(film.data);
+  const onWatchedClick = async () => {
+    ID = openModal(e);
+    console.log(ID);
+    const data = await searchDetails(ID);
+    if (!moviesWatched.find(item => item.id === ID)) {
+      moviesWatched.push(data);
       localStorage.setItem('movies-watched', JSON.stringify(moviesWatched));
       addToWatched();
       return;
     }
-    let index = moviesWatched.findIndex(object => object.id === film.data.id);
+    let index = moviesWatched.findIndex(object => object.id === ID);
     moviesWatched.splice(index, 1);
     localStorage.setItem('movies-watched', JSON.stringify(moviesWatched));
     removeFromWatched();
-  }
-  function onQueueClick() {
-    if (!moviesQueue.find(item => item.id === film.data.id)) {
-      moviesQueue.push(film.data);
+  };
+  const onQueueClick = async () => {
+    ID = openModal();
+    console.log(ID);
+    const data = await searchDetails(ID);
+    if (!moviesQueue.find(item => item.id === ID)) {
+      moviesQueue.push(data);
       localStorage.setItem('movies-queue', JSON.stringify(moviesQueue));
       addToQueue();
       return;
     }
-    let index = moviesQueue.findIndex(object => object.id === film.data.id);
+    let index = moviesQueue.findIndex(object => object.id === ID);
     moviesQueue.splice(index, 1);
     localStorage.setItem('movies-queue', JSON.stringify(moviesQueue));
     infoRemoveFromQueue();
-  }
+  };
 };

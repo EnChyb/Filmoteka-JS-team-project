500a94361fbeb2589852730de24ff54d5424b83d
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-03-18 15:57:30 +0100

    upd

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 import './js/database.js';
+import './js/modal-team.js';
 import './js/modal.js';
 import './js/pagination.js';
-import './js/modal-team.js';
+import './js/queue.js';
 import './js/scroll-top_bottom.js';
+import './js/watched.js';
 
 //import '';
